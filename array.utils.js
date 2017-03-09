896e3d5622af6755804341c4506ececf57d5fe45
Phlippe Moneda <philippemoneda@gmail.com>
2017-03-09 13:58:58 -0300

    add asyncEach

--- array.utils.js
+++ array.utils.js
@@ -240,3 +240,29 @@ Array.prototype.distinct = function (key) {
     }
     return list;
 };
+
+
+Array.prototype.asyncEach = function (callback,done) {
+    var current = 0;
+    var arr = this;
+    function next(){
+        current++;
+        if(current < arr.length){
+           typeof callback === "function" && callback(arr[current],next,stop);
+        }else{
+           typeof done === "function" && done();
+        }
+    }
+    var stoped = false;
+    function stop(status, error){        
+        stoped = true;
+    }
+    if(stoped) return;
+    
+    if(arr.length === 0) {
+        typeof done === "function" && done();
+        return;
+    }
+    
+    typeof callback === "function" && callback(this[current],next,stop);
+};
